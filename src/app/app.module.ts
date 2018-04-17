70834f21222ca9c4426a72ed73a858bb7bf93dcc
Naveen Paul <naveenpaul.markunda@gmail.com>
2018-04-17 13:06:17 +0530

    Relatas new framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AboutModule } from './about/about.module';
 import { LoginModule } from './login/login.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
@@ -30,7 +31,7 @@ import { AppRoutingModule } from './app-routing.module';
     LoginModule,
     AppRoutingModule
   ],
-  declarations: [AppComponent],
+  declarations: [AppComponent, FooterComponent],
   providers: [
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,12 +6,45 @@ import { QuoteService } from './quote.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss', '../footer/footer.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   quote: string;
   isLoading: boolean;
+
+  cxoIsSelected = {
+    'arrow_box_bottom': true,
+    'orange-invert-btn': true
+  };
+
+  managerIsSelected = {
+    'arrow_box_bottom': false,
+    'orange-invert-btn': true
+  };
+
+  repIsSelected = {
+    'arrow_box_bottom': false,
+    'orange-invert-btn': true
+  };
+
+  openCxo() {
+    this.cxoIsSelected.arrow_box_bottom = true;
+    this.managerIsSelected.arrow_box_bottom = false;
+    this.repIsSelected.arrow_box_bottom = false;
+  }
+
+  openManager() {
+    this.managerIsSelected.arrow_box_bottom = true;
+    this.cxoIsSelected.arrow_box_bottom = false;
+    this.repIsSelected.arrow_box_bottom = false;
+  }
+
+  openRep() {
+    this.repIsSelected.arrow_box_bottom = true;
+    this.managerIsSelected.arrow_box_bottom = false;
+    this.cxoIsSelected.arrow_box_bottom = false;
+  }
 
   constructor(private quoteService: QuoteService) { }
 
